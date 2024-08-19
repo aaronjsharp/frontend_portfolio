4c4d7f074edf7fa639cf6eb9d0f760ac7ad52f21
aaronjsharp <aaron.sharp2011@gmail.com>
2024-08-19 10:21:28 -0500

    complete nav section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
+import { Inter, Calistoga } from 'next/font/google'
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans'})
+
+const calistoga = Calistoga({ subsets: ['latin'], variable: '--font-serif', weight: ['400']})
 
 export const metadata: Metadata = {
   title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  description: "Created Aaron Sharp",
 };
 
 export default function RootLayout({
@@ -13,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={twMerge(
+        "bg-gray-900 text-white antialiased font-sans",
+        inter.variable, calistoga.variable
+      )}>{children}</body>
     </html>
   );
 }
