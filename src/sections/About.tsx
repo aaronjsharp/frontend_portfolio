017095c26259f44d8e8ca5c6b0bc4e4b71552df2
aaronjsharp <aaron.sharp2011@gmail.com>
2024-08-26 12:38:34 -0500

    Complete framer motion animation in the about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,6 +1,7 @@
+'use client'
+
 import { Card } from '@/components/Card';
 import { SectionHeader } from '@/components/SectionHeader';
-import StarIcon from '@/assets/icons/star.svg';
 import bookImage from '@/assets/images/book-cover.png';
 import JavascriptIcon from '@/assets/icons/square-js.svg';
 import HTMLIcon from '@/assets/icons/html5.svg';
@@ -10,11 +11,12 @@ import ChromeIcon from '@/assets/icons/chrome.svg';
 import GithubIcon from '@/assets/icons/github.svg';
 import mapImage from '@/assets/images/map.png';
 import smileMemoji from '@/assets/images/memoji-smile.png';
-
-import Image from 'next/image';
-import { TechIcon } from '@/components/TechIcon';
 import { CardHeader } from '@/components/CardHeader';
 import { ToolboxItems } from '@/components/ToolboxItems';
+
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { useRef } from 'react';
 
 const toolboxItems = [
   {
@@ -90,6 +92,8 @@ const hobbies = [
 ];
 
 export const AboutSection = () => {
+  const constraintRef = useRef(null)
+
   return (
     <div className='py-20 lg:py-28'>
       <div className='container'>
@@ -133,21 +137,23 @@ export const AboutSection = () => {
                 className='py-6 px-6'
               />
 
-              <div className='relative flex-1'>
+              <div className='relative flex-1' ref={constraintRef}>
                 {hobbies.map((hobby) => (
-                  <div
+                  <motion.div
                     key={hobby.title}
                     className='inline-flex gap-2 px-6 bg-gradient-to-r from-emerald-200 to-sky-400 rounded-full py-1.5 absolute'
                     style={{
                       left: hobby.left,
                       top: hobby.top,
                     }}
+                    drag
+                    dragConstraints={constraintRef}
                   >
                     <span className='font-medium text-gray-950'>
                       {hobby.title}
                     </span>
                     <span>{hobby.emoji}</span>
-                  </div>
+                  </motion.div>
                 ))}
               </div>
             </Card>
