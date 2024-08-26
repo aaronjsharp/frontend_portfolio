17369a016ce7692e180b48341b008013e54599af
aaronjsharp <aaron.sharp2011@gmail.com>
2024-08-26 10:21:48 -0500

    Add ping animation in Hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -60,7 +60,9 @@ export const HeroSection = () => {
             width={100}
           />
           <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg'>
-            <div className='bg-green-500 size-2.5 rounded-full' />
+            <div className='relative bg-green-500 size-2.5 rounded-full ' >
+              <div className='absolute inset-0 bg-green-500 size-2.5 rounded-full animate-ping-large' />
+            </div>
             <div className='text-sm font-medium'>
               Available for new projects
             </div>
